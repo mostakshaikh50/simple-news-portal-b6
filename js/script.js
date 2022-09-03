e68e314eceabd1a19017cca35a1058b813381939
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-09-03 09:46:35 +0100

    category added dynamically

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-function fetchData(){
-    fetch('https://openapi.programming-hero.com/api/news/categories');
+// function fetchData(){
+//     fetch('https://openapi.programming-hero.com/api/news/categories')
+//     .then(response =>{
+//         console.log(response);
+//     });
+// }
+
+// fetchData();
+
+const loadCategories = () =>{
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+    .then(response => response.json())
+    .then(data => displayCategories(data.data.news_category))
 }
 
-fetchData();
+const displayCategories = newsCategories =>{
+    //console.log(newsCategory);
+    const newsCategoriesList =document.getElementById('news-categories');
+     for(const newsCategory of newsCategories){        
+        const newsCategoryName = document.createElement('div');
+        newsCategoryName.classList.add('category');
+        newsCategoryName.innerHTML = `
+        <li><a>${newsCategory.category_name}</a></li>`;
+        newsCategoriesList.appendChild(newsCategoryName);
+     }
+     
+}
+
+loadCategories();
