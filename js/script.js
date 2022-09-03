e673ac83a6cc288bebf796403dc67d0807aeb874
mostakshaikh50 <mostak.50.cse@gmail.com>
2022-09-03 13:29:36 +0100

    category details added

--- js/script.js
+++ js/script.js
@@ -14,16 +14,62 @@ const loadCategories = () =>{
 }
 
 const displayCategories = newsCategories =>{
-    //console.log(newsCategory);
+    //console.log(newsCategories);
     const newsCategoriesList =document.getElementById('news-categories');
-     for(const newsCategory of newsCategories){        
+
+    newsCategories.forEach(newsCategory =>{
         const newsCategoryName = document.createElement('div');
         newsCategoryName.classList.add('category');
         newsCategoryName.innerHTML = `
-        <li><a>${newsCategory.category_name}</a></li>`;
+        <li onclick="categoryDetails('${newsCategory.category_id}')" id="${newsCategory.category_id}"><a>${newsCategory.category_name}</a></li>`;
         newsCategoriesList.appendChild(newsCategoryName);
-     }
+    })
+
+    //  for(const newsCategory of newsCategories){        
+    //     const newsCategoryName = document.createElement('div');
+    //     newsCategoryName.classList.add('category');
+    //     newsCategoryName.innerHTML = `
+    //     <li onclick="categoryDetails('${newsCategory.category_id}')" id="${newsCategory.category_id}"><a>${newsCategory.category_name}</a></li>`;
+    //     newsCategoriesList.appendChild(newsCategoryName);
+    //  }
      
 }
+
+const categoryDetails = category_id => {
+    console.log(category_id);
+    fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+    .then(response => response.json())
+    .then(data => displayCategoryDetails(data.data))
+}
+
+const displayCategoryDetails = categoryDetails =>{
+    console.log(categoryDetails)
+     const newsCategoriesDetails =document.getElementById('category-details');
+     newsCategoriesDetails.innerHTML ='';
+     categoryDetails.forEach(newsDetails =>{
+        const newsDetailsDiv = document.createElement('div');
+        newsDetailsDiv.classList.add('newsDetails');
+        newsDetailsDiv.classList.add('card-body');
+        newsDetailsDiv.classList.add('card-title');
+        newsDetailsDiv.classList.add('card-actions');
+        newsDetailsDiv.classList.add('justify-end');
+       // newsDetailsDiv.classList.add('btn btn-primary');
+        
+        newsDetailsDiv.innerHTML = `
+        <img src="${newsDetails.thumbnail_url}" alt="Album">
+        <div class="card-body">
+          <h2 class="card-title">${newsDetails.title}</h2>
+          <p>${newsDetails.details}</p>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary">Listen</button>
+          </div>
+        </div>`;
+
+        newsCategoriesDetails.appendChild(newsDetailsDiv);
+       
+     })
+     
+}
+
 
 loadCategories();
